9bc5b8707f1cda1eb8355ce7fa27748739bd7477
abhishek <abhishekp4500@gmail.com>
2026-04-12 17:46:17 +0530

    http:method get put post delete...

--- src/app.js
+++ src/app.js
@@ -2,12 +2,22 @@ const express = require("express");
 
 const app= express();
 
-app.use("/",(req,res) =>{
-res.send("Welcome to dashbord");
+// routing creation 
+//order of routes matter
+// app.use("/test",(req,res) =>{
+// res.send("no test cases");
+// });
+
+app.get("/user",(req,res) =>{
+res.send({firstname:"abhishek",lastname:"kumar"});
 });
 
-app.use("/test",(req,res) =>{
-res.send("no test cases");
+app.post("/user",(req,res) =>{
+res.send("data saved successfully to database");
+});
+
+app.delete("/user",(req,res) =>{
+res.send("data deleted successfully from database");
 });
 
 app.listen(1000,()=>{
